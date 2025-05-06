22708757cc467f3cb796ea30d55bdf207fde4a74
smG <mikki3232go@gmail.com>
2025-05-06 23:22:57 +0900

    interface update

--- src/CardList.tsx
+++ src/CardList.tsx
@@ -1,23 +1,12 @@
 import "./styles.css";
-import roomDetails from "./Rooms";
+import Rooms, { Room } from "./Rooms";
 import Card from "./components/Card";
-
-interface Room {
-  key: number;
-  name: string;
-  location: string;
-  totalGuest: string;
-  rating: string;
-  numberOfRating: string;
-  price: string;
-  image: string;
-}
 
 export default function CardList() {
   return (
     <div>
       <h2>Your result is here!</h2>
-      {roomDetails.map((item: Room) => (
+      {Rooms.map((item: Room) => (
         <Card key={item.key} item={item} />
       ))}
     </div>
